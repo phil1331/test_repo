13290bec7b642123d3dc3cf4e8f7e51fe40744d3
Philipp Barski <philipp.barski@berlin.de>
2021-06-23 10:41:38 +0200

    Kleiner ipconfig, Commit nr 2

--- Test.java
+++ Test.java
@@ -1,4 +1,21 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedInputStream;
+
 public class Test {
+	
+	
+	public static void main (String [] args) throws IOException{
+		
+		
+		final Process p = Runtime.getRuntime().exec("ipconfig /ALL");
+		final BufferedReader br = new BufferedReader(new InputStreamReader(new BufferedInputStream(p.getInputStream())));
+		for(String s; (s = br.readline()) != null;) {
+			System.out.println(s);
+		}
+		
+	}
 
 
 }
