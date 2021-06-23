da08446e21979b6c37b4c918c26715d91ed85736
Philipp Barski <philipp.barski@berlin.de>
2021-06-23 12:05:56 +0200

    Test java code, Data-Reader

--- Test.java
+++ Test.java
@@ -5,7 +5,6 @@ import java.io.BufferedInputStream;
 
 public class Test {
 	
-	
 	public static void main (String [] args) throws IOException{
 		
 		final Process p = Runtime.getRuntime().exec("ipconfig /ALL");
