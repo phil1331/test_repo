e278e025eb2091f9754a13a3501e318b8f269688
phil1331 <83304589+phil1331@users.noreply.github.com>
2021-06-23 12:08:03 +0200

    Simple java data-reader

--- Test.java
+++ Test.java
@@ -7,7 +7,7 @@ public class Test {
 	
 	public static void main (String [] args) throws IOException{
 		
-		final Process p = Runtime.getRuntime().exec("ipconfig /ALL");
+		final Process p = Runtime.getRuntime().exec("ipconfig /all");
 		final BufferedReader br = new BufferedReader(new InputStreamReader(new BufferedInputStream(p.getInputStream())));
 		for(String s; (s = br.readLine()) != null;) {
 			System.out.println(s);
